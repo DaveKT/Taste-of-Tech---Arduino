abf1b7903f9224af7c66efae7c3ec3cacaf3104d
David Tassara <phi.fibonacci@gmail.com>
2017-02-22 16:08:21 -0800

    Adding a file the demonstrates using sin and linear interpolation to transition smoothly between two values. This version has a bug that causes a skip.

---
+++ Curve/Curve.ino
@@ -0,0 +1,29 @@
+
+const float pi = 3.1415;
+const float FREQUENCY = 1.0;
+int current;
+float x, outV;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  current = millis()*10;
+  x = sin(2.0*pi*FREQUENCY*current);
+  outV = myMap(x, -1.0, 1.0, 1, 100);
+
+  for(int i=0; i<int(outV); i++) {
+    Serial.print("*");
+  }
+  Serial.print(outV);
+  Serial.println();
+  
+  delay(10);
+}
+
+float myMap(float v, float x0, float x1, float y0, float y1) {
+  return y0 + (v-x0)*((y1-y0)/(x1-x0));
+}
+
