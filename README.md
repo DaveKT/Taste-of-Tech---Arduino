49a679197de57bb27c886e6cd2effb5566b97612
David Tassara <phi.fibonacci@gmail.com>
2018-05-31 10:00:55 -0700

    Added content for the slide switch.

--- README.md
+++ README.md
@@ -122,7 +122,6 @@ Prior to starting the class make sure that the computers all have the Arduino ID
 This section includes links to additional programs that demonstrate various components of the circuit playground board. Full outlines are not available but the code is commented.
 
 * [Button Test](docs/ButtonTest/ButtonTest.ino), demonstrates the use of circuit playground buttons.
-* Accelerometer Graph
-* Slide Switch Test
+* [Slide Switch Test](docs/SlideSwitchTest/SlideSwitchTest.ino), demonstrates the use of the slide switch.
 * Fire
 * Curve
--- docs/SlideSwitchTest/SlideSwitchTest.ino
+++ docs/SlideSwitchTest/SlideSwitchTest.ino
@@ -1,9 +1,15 @@
+//Purpose: Demonstrate the use of the slide switch.
+
+//import the circuit playground library
 #include <Adafruit_CircuitPlayground.h>
 
+//used to keep track of the slide switch position
 bool current;
 
 void setup() {
 
+  //Enable Serial (for printing), start Circuit Playground, and
+  //get the state of the slide switch
   Serial.begin(9600);
   CircuitPlayground.begin();
   current = CircuitPlayground.slideSwitch();
@@ -12,12 +18,14 @@ void setup() {
 
 void loop() {
 
+  //check the state of the slideswitch and print which side it's on
   if(!current == CircuitPlayground.slideSwitch()) {
     if(CircuitPlayground.slideSwitch()) {
       Serial.println("Switch is on the left.");
     } else {Serial.println("Switch is on the right.");}
   }
 
+  //update the current position
   current = CircuitPlayground.slideSwitch();
   delay(100);
 
